0faeb0f722c06566d6fe8d85d7980a5f9a7e0d03
K Shyam Kumar Reddy <kshyamrdy@gmail.com>
2024-04-07 17:06:24 +0530

    #

---
+++ LeetCode/678. Valid Parenthesis String.py
@@ -0,0 +1,82 @@
+"""
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "(*)"
+Output: true
+Example 3:
+
+Input: s = "(*))"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i] is '(', ')' or '*'.
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "(*)"
+Output: true
+Example 3:
+
+Input: s = "(*))"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i] is '(', ')' or '*'.
+
+
+"""
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        open_balance = 0
+        # Forward Pass
+        for i in s:
+            if i in '(*':
+                open_balance += 1
+            elif open_balance:
+                open_balance -= 1
+            else:
+                return False
+        
+        # Backward pass
+        closed_balance = 0
+        for i in s[::-1]:
+            if i in ')*':
+                closed_balance += 1
+            elif closed_balance:
+                closed_balance -= 1
+            else:
+                return False
+        return True
