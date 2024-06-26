fee79351e694951dbdd2297204dc7e5ae55a9468
K Shyam Kumar Reddy <kshyamrdy@gmail.com>
2024-06-26 22:45:58 +0530

    #

---
+++ LeetCode/1382. Balance a Binary Search Tree.py
@@ -0,0 +1,61 @@
+"""
+Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
+
+A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,null,3,null,4,null,null]
+Output: [2,1,3,null,null,null,4]
+Explanation: This is not the only correct answer, [3,1,4,null,2] is also correct.
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,1,3]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+1 <= Node.val <= 105
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        inorder = []
+
+        def traverse(root):
+            if not root:
+                return None
+            
+            traverse(root.left)
+            inorder.append(root.val)
+            traverse(root.right)
+        traverse(root)
+
+        N = len(inorder)
+
+        def construct(left, right):
+            if left > right:
+                return None
+            
+            if left == right:
+                return TreeNode(inorder[left])
+
+            mid = (left + right)//2
+            node = TreeNode(inorder[mid])
+            node.left = construct(left, mid-1)
+            node.right = construct(mid+1, right)
+            return node
+        return construct(0, N-1)
